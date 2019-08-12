5575ec1b10878c2cf5e886539294130882f87b68
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-08-12 17:54:55 +0100

    rails create action lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: [:index, :new, :create]
-  get '/student/:id', to: 'students#show', as: 'student'
+  
+
+  #index
+  get '/students', to: 'students#index', as: "students"
+  #new
+  get '/students/new', to: 'students#new', as: 'new_student'
+  #show
+  get '/students/:id', to: 'students#show', as: 'student'
+  #create
+  post '/students', to: 'students#create'
+  #edit
+  get '/students/:id/edit', to: 'students#edit', as: 'edit_student'
+  #update
+  patch '/students/:id', to: 'students#update'
+  #destroy
+  delete '/students/:id', to: 'students#destroy'
 end
